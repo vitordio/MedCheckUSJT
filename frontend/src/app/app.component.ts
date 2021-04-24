39478ffa2ed21dff697a3f4b43caaada1812e34c
Gabriel Venancio Campaner <gabvcampaner@gmail.com>
2021-04-24 10:42:07 -0300

    Criação da interface - Frontend

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+
+  paciente = [
+    {nomePaciente: "Sanji", cpfPaciente: "12345678910", leitoPaciente: "B23", datainternPaciente: "24/10/2001", dataaltaPaciente:"23/04/2021"}
+  ];
+  
+  cadastrar (nomePaciente: any, cpfPaciente: any, leitoPaciente: any, datainternPaciente: any, dataaltaPaciente: any){
+    this.paciente = [...this.paciente, {nomePaciente:nomePaciente, cpfPaciente:cpfPaciente, leitoPaciente:leitoPaciente, datainternPaciente:datainternPaciente, dataaltaPaciente:dataaltaPaciente}];
+  }
 }
